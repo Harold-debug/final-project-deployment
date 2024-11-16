1d082c3b4aadac1030d19c08994a8716dd414ee7
Harold Geumtcheng <geumtchengh@gmail.com>
2024-11-16 16:48:01 +0100

    Disable debug mode in app.py for production deployment

--- app/app.py
+++ app/app.py
@@ -253,4 +253,4 @@ def serve_runs(filename):
     
 if __name__ == '__main__':
     # app.run(debug=True)
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
