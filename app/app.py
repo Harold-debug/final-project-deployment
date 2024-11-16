e90c93827f6d212aa182abb1b6719c75ec704f6b
Harold Geumtcheng <geumtchengh@gmail.com>
2024-11-16 14:37:00 +0100

    Update app.py to enable production mode by configuring host and port settings

--- app/app.py
+++ app/app.py
@@ -252,5 +252,5 @@ def serve_runs(filename):
         return "Error serving file", 500
     
 if __name__ == '__main__':
-    app.run(debug=True)
-    # app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
